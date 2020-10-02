82459c840aa320af56e06092d3b82a24364a1c36
necopolis <39461844+necopolis@users.noreply.github.com>
2020-10-01 22:23:10 -0300

    Subiendo!!!

---
+++ src/testcolegio/Alumno.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testcolegio;
+
+import java.util.HashSet;
+
+import java.util.Set;
+
+/**
+ *
+ * @author MArio
+ */
+public class Alumno {
+    //Atributos
+    private int legajo;
+    private String apellido;
+    private String nombre;
+    private Set <Materia> listaM;
+    //Metodos
+    public Alumno(int legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.listaM=new HashSet<>();
+    }
+    public void agregarMateria(Materia materia){
+        listaM.add(materia);
+    }
+    public int cantidadMaterias(){
+    
+        return listaM.size();
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Set<Materia> getListaM() {
+        return listaM;
+    }
+
+    public void setListaM(Set<Materia> listaM) {
+        this.listaM = listaM;
+    }
+    public void mostrar(){
+        for(Materia it: this.getListaM()){
+            it.mostrar();
+        }
+    }
+    
+}
---
+++ src/testcolegio/TestColegio.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testcolegio;
+
+import java.util.Set;
+
+/**
+ *
+ * @author MArio
+ */
+public class TestColegio {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Ingles 1 de primer año - 0120  //Matematicas de primer año - 0130
+        //Laboratorio 1 de primer año - 0140
+        Materia m1= new Materia("0120", "Ingles",1);
+        Materia m2= new Materia("0130", "Matematica",1);
+        Materia m3= new Materia("0140", "Laboratorio",1);
+                    //Repetidos especiales
+                    //distinto nombre, pero = id
+            //Materia m4= new Materia("0120", "Probemos",1);
+                     //distinto id = nombre
+            //Materia m5= new Materia("0150", "Laboratorio",1);
+        Alumno a1=new Alumno(1001, "Lopez", "Martin");
+        Alumno a2=new Alumno(1002, "Martinez", "Brenda");
+        a1.agregarMateria(m1);
+        a1.agregarMateria(m2);
+        a1.agregarMateria(m3);
+        a2.agregarMateria(m1);
+        a2.agregarMateria(m2);
+        a2.agregarMateria(m3);
+        a2.agregarMateria(m3);
+                //probando ingresar repetidos especiales
+        //a2.agregarMateria(m4);
+        //a2.agregarMateria(m5);
+        System.out.println("La cantidad de materias de "+ a1.getApellido()+" es: "+ a1.cantidadMaterias());
+        a1.mostrar();
+        System.out.println("---------------------*****-------------------");
+        System.out.println("La cantidad de materias de "+ a2.getApellido()+" es: "+ a2.cantidadMaterias());
+        a2.mostrar();
+    }   
+    
+}
